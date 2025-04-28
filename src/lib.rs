91b06f807658df79808427aecb5dc81656d41f04
James Bird <jbrd@users.noreply.github.com>
2025-04-28 17:08:45 +0100

    #52 Fix doctests

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,14 @@
 //!
 //! fn main() {
 //!     let mut app = App::new();
-//!     app.insert_resource(ClearColor(Color::rgba(0.2, 0.2, 0.2, 1.0)))
+//!     app.insert_resource(ClearColor(Color::srgba(0.2, 0.2, 0.2, 1.0)))
 //!         .insert_resource(ImguiState {
 //!             demo_window_open: true,
 //!         })
 //!         .add_plugins(DefaultPlugins)
 //!         .add_plugins(bevy_mod_imgui::ImguiPlugin::default())
 //!         .add_systems(Startup, |mut commands: Commands| {
-//!             commands.spawn(Camera3dBundle::default());
+//!             commands.spawn(Camera3d::default());
 //!         })
 //!         .add_systems(Update, imgui_example_ui);
 //!     app.run();
